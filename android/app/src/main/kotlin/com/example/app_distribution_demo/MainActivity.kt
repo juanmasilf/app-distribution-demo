47f07e7dd54f1cf4a9e56aa13075d9a958548dfb
juanmasilf <juanmasilveira@hotmail.com>
2021-10-04 23:30:47 -0300

    Version 1.0.0

---
+++ android/app/src/main/kotlin/com/example/app_distribution_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_distribution_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // Initialize Firebase.
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    Firebase.initializeApp();
+    return MaterialApp(
+      title: 'App Distribution Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App Distribution demo"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.network(
+              "https://www.gstatic.com/alkali/eba17e954431c80b7b31125928f3379476ee4934.png",
+              width: 200,
+            ),
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Welcome to version 1.0.0!!",
+                style: Theme.of(context).textTheme.headline4,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            ElevatedButton(
+                child: Text("Crash the app"),
+                onPressed: () => FirebaseCrashlytics.instance.crash())
+          ],
+        ),
+      ),
+    );
+  }
+}
